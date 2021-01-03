9201ae67952e086e1312f3ffbeb5670c0322a376
rps1509 <suryanshrpsingh@live.com>
2021-01-03 09:33:54 +0530

    Initial Project Commit
    Main Project with implementation of Simple & Anonymous Function Delegates.

---
+++ myCSharpConcepts/Delegate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCSharpConcepts
+{
+    public delegate void PrintMessage(string msg);  //Simple Delegates
+
+    public delegate int Math(int a, int b);     //Anonymous Method or Returned type is not void
+    public class Delegate
+    {
+        static void Main(string[] args)
+        {
+            //Simple Delegates
+            PrintMessage ob = PrintMsg;
+            ob("Print this message 1"); //If void return type then can be called as many times without creating new instance
+            ob("Print this message 2");
+            ob("Print this message 3");
+
+
+            //Anonymous Method delegates
+            int a, b;
+            Console.WriteLine("Enter Value of A");
+            a = Convert.ToInt32(Console.ReadLine());   // Get First Value from User
+            Console.WriteLine("Enter Value of B");
+            b = Convert.ToInt32(Console.ReadLine());   // Get Second Value from User
+
+            //sum instance of delegate
+            Math sum = delegate (int c, int d) { return c + d; };
+            Console.WriteLine($"Sum is {sum(a,b)}"); //Invoke Sum Delegate with user values
+
+            //subtract instance of delegate
+            Math subtract = delegate (int c, int d) { return c - d; };
+            Console.WriteLine($"Subtraction is {subtract(a, b)}");   //Invoke Subtract Delegate with user values
+
+            //multiply instance of delegate
+            Math multiply = delegate (int c, int d) { return c * d; };
+            Console.WriteLine($"Multiply is {multiply(a, b)}"); //Multiply Subtract Delegate with user values
+
+            //divide instance of delegate
+            Math divide = delegate (int c, int d) { return c / d; };
+            Console.WriteLine($"Divide is {divide(a, b)}"); //Invoke Divide Delegate with user values
+
+            Console.WriteLine("Enter any key to exit...");
+            Console.ReadLine();
+        }
+
+        public static void PrintMsg(string message)
+        {
+            Console.WriteLine("Delegate invoked with : " + message);
+        }
+    }
+}
---
+++ myCSharpConcepts/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCSharpConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<string>
+            {
+                "\n There are multiple C# Classes in this project with Implementation and Clear Examples",
+                "\n To Debug/Test any specific class file, follow below steps",
+                "\n STEP 1: Goto Project > Properties",
+                "\n STEP 2: Application > Startup Object",
+                "\n STEP 3: Select Class file name > Save",
+                "\n STEP 4: Debug \n"
+            };
+
+            Console.WriteLine(" \n Welcome to C Sharp Concepts by Raghav");
+
+            Console.WriteLine(string.Join(Environment.NewLine, items));
+            printStars();
+
+            Console.WriteLine("\n Enter any key to exit..");
+            Console.ReadKey();
+        }
+
+        public static void printStars()
+        {
+            int number, i, k, count = 1;
+            number = 7;
+            count = number - 1;
+            for (k = 1; k <= number; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * k - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            count = 1;
+            for (k = 1; k <= number - 1; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - k) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}

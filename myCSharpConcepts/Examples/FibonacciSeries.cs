73ecfaa21867a2253b33e1c58a79ee6cc279550d
rps1509 <suryanshrpsingh@live.com>
2021-01-25 21:22:08 +0530

    New Examples

---
+++ myCSharpConcepts/Examples/FibonacciSeries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCSharpConcepts.Examples
+{
+    public class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number till you want fibonacci");
+            int maxVal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Fibonacci series till {maxVal} are");
+
+            foreach(int i in fibonacci(maxVal))
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+
+        public static IEnumerable<int> fibonacci(int maxval)
+        {
+            int series = 0;
+            for(int i=0; i < maxval; i++)
+            {
+                yield return series + i;
+            }
+        }
+    }
+}
---
+++ myCSharpConcepts/Examples/MultipleAwait.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCSharpConcepts.Examples
+{
+    class MultipleAwait
+    {
+        static async Task Main(string[] args)
+        {
+           await Calling();
+                // Do any async anything you need here without worry
+         
+
+            Console.ReadKey();
+        }
+
+       static async Task Calling()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("First");
+
+
+            await Task.Delay(3000);
+            Console.WriteLine("secod");
+
+
+            await Task.Delay(1000);
+            Console.WriteLine("third");
+        }
+    }
+}
